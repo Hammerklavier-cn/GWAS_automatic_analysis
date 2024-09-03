0e57156d72d5bb29779393173bfa5afe16dc0778
hammerklavier <hammerklavier@noreply.gitcode.com>
2024-09-03 16:18:53 +0800

    添加相关性分析功能

---
+++ myutil/association_analysis.py
@@ -0,0 +1,56 @@
+import logging
+import subprocess
+import sys
+
+
+def quantitive_association(
+    plink_path: str,
+    input_name: str,
+    phenotype_name: str,
+    phenotype_info_path: str,
+    output_name: str
+) -> None:
+    """
+    Performs a quantitative association analysis on the given input file.
+    
+    The function uses the plink command-line tool to perform the analysis.
+    
+    It will generate a association result file named `output_name.qaaoc`
+    
+    Args:
+        plink_path (str): 
+            Path to the plink executable file.
+        input_name (str): 
+            Name of the input file.
+        phenotype_name (str):
+            Name of the phenotype.
+        phenotype_info_path (str):
+            Path to the phenotype information file.
+        output_name (str):
+            Name of the output file.
+    """
+    logging.info("Performing quantitative association analysis...")
+    try:
+        command = [
+            plink_path,
+            "--bfile", input_name,
+            "--pheno", phenotype_info_path,
+            "--assoc",
+            "--out", output_name
+        ]
+        subprocess.run(
+            command,
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=None        
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while running plink: {e}")
+        sys.exit(2)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        sys.exit(-3)
+    logging.info("Quantitative association analysis completed.")
+    
+        
+    
