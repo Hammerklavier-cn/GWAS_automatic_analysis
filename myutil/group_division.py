23dc812342f375f9e7cc347a3c666606a453dcfb
hammerklavier <q5vsx3@163.com>
2025-04-01 22:35:23 +0800

    Add blank line after logger initialization in group_division.py

--- myutil/group_division.py
+++ myutil/group_division.py
@@ -12,6 +12,7 @@ from typing import Optional
 from myutil.small_tools import create_logger
 
 logger = create_logger("GroupDivisionLogger", level=logging.WARN)
+
 def divide_pop_by_ethnic(
         plink_path: str,
         input_name: str,
