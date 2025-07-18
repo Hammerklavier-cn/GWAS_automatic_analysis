6f6114f38e694399c990b8509513aaf3b0f285e9
hammerklavier <q5vsx3@163.com>
2025-07-18 19:40:37 +0800

    Update input paths and output directory in result analysis script
    The input paths now point to MRC1-clean_ethnic-results and the output
    directory is renamed to results_clean-ethnic. Also added reference.tsv
    as a new required input parameter.

--- toolkit/result_analysis/run.sh
+++ toolkit/result_analysis/run.sh
@@ -1,6 +1,7 @@
 python3 main.py \
-    -i ../../results/MRC1_part1_split-gender/summary-q_adjusted.tsv \
-        ../../results/MRC1_part2_split-gender/summary-q_adjusted.tsv \
-        ../../results/MRC1_part3_split-gender/summary-q_adjusted.tsv \
+    -i /home/jimmy/projects/GWAS_automatic_analysis/results/MRC1-clean_ethnic-results/MRC1-part1-split_gender/summary-q_adjusted.tsv \
+        /home/jimmy/projects/GWAS_automatic_analysis/results/MRC1-clean_ethnic-results/MRC1-part2-split_gender/summary-q_adjusted.tsv \
+        /home/jimmy/projects/GWAS_automatic_analysis/results/MRC1-clean_ethnic-results/MRC1-part3-split_gender/summary-q_adjusted.tsv \
+    --reference run/reference.tsv \
     -a all \
-    -o results_split-gender
+    -o results_clean-ethnic
